eb5fe1f7438c79d67b6491abc0704145786e03e9
Aman Kumar <aman.kumar@gkcfoods.com>
2025-10-08 20:06:08 +0530

    seal site added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Art_Ibro';
+  title = 'ART iBrow Threading Salon';
+  public web: any;
+
+  ngOnInit() {
+    this.loadSiteSeal();
+  }
+
+  loadSiteSeal() {
+    if (this.web === "ART iBrow Threading Salon") {
+      const script = document.createElement('script');
+      script.type = 'text/javascript';
+      script.src = 'https://www.rapidscansecure.com/siteseal/siteseal.js?code=64,BA813700E0ED8297781076AC01F939D228A524E4';
+      script.async = true;
+      document.body.appendChild(script);
+
+      script.onload = () => {
+        console.log("Site Seal Script Loaded for ART iBrow Threading Salon");
+      };
+    }
+  }
+
 }
